0493ee45bd39593ba63194e9e83f0ab8b47d7f8d
Mando Acosta <badkill@live.com>
2025-07-25 18:10:12 -0600

    Update Utils.js

--- Utils.js
+++ Utils.js
@@ -1,29 +1,29 @@
-// Utils DOM.js - Librería reutilizable
+// Utils.js - Librería reutilizable
 
 
 (function (global) {
     'use strict';
 
     const utilsDOM = {
-        qSelector(selector) {
+        const qSelector(selector) {
             return document.querySelector(selector);
-        },
+        };
 
-        qSelecID(selector) {
+        const qSelecID(selector) {
             return document.getElementById(selector);
-        },
+        };
 
-        qSelectors(selector) {
+        const qSelectors(selector) {
             return document.querySelectorAll(selector);
-        },
+        };
 
-        activarElemento(selector) {
+        const activarElemento(selector) {
             const el = document.querySelector(selector);
             if (el) el.focus();
             return el;
-        },
+        };
 
-        seleccionarElemento(selector) {
+        const seleccionarElemento(selector) {
             const el = document.querySelector(selector);
             if (el && el.select) {
                 el.select();
@@ -35,30 +35,38 @@
                 sel.addRange(range);
             }
             return el;
-        },
+        };
 
-        scrollElemento(selector, options = { behavior: "smooth", block: "center" }) {
+        const scrollElemento(selector, options = { behavior: "smooth", block: "center" }) {
             const el = document.querySelector(selector);
             if (el) el.scrollIntoView(options);
             return el;
-        },
+        };
 
-	activateAndScroll(selector, options = { behavior: "smooth", block: "center" }) {
+	const activateAndScroll(selector, options = { behavior: "smooth", block: "center" }) {
         if (selector) {
             //selector.disabled = false;
             selector.scrollIntoView(options);
             selector.focus();
             if (selector.select) selector.select();
         }
-    	},
+    	};
 
-	function isMobile() {
+	const isMobile() {
             const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
             return regex.test(navigator.userAgent);
-	}
-    };
+	};
 
     // Exportar como global para usar en otros scripts
-    global.utilsDOM = utilsDOM;
+    global.Utils = {
+    qSelector,
+    qSelecID,
+    qSelectors,
+    activarElemento,
+    seleccionarElemento,
+    scrollElemento,
+    activateAndScroll,
+    isMobile
+  };
 
 })(window);
